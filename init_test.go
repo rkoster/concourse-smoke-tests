fcd88614bbbad0ccfab6efdb52819e0e04a8f10c
Josh Zarrabi <jzarrabi@pivotal.io>
2018-11-02 10:42:44 -0400

    add ci task to run tests in concourse
    - also run dep ensure

--- init_test.go
+++ init_test.go
@@ -4,9 +4,11 @@
 package smoke_tests
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -14,40 +16,61 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
+	yaml "gopkg.in/yaml.v2"
 )
 
 const flyTarget = "st"
 
-var (
-	concourseUrl string
-	username     string
-	password     string
-
-	flyPath           string
-	skipSSLValidation bool
-)
+var source ConcourseSource
 
 func TestSmoke(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "concourse-smoke-tests")
 }
 
+type LocalUser struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+type ConcourseSource struct {
+	ExternalURL       string    `yaml:"external_url"`
+	LocalUser         LocalUser `yaml:"local_user"`
+	SkipSSLValidation bool      `yaml:"skip_ssl_validation"`
+	FlyPath           string    `yaml:"fly_path"`
+}
+
 var _ = BeforeSuite(func() {
-	concourseUrl = os.Getenv("CONCOURSE_URL")
-	if concourseUrl == "" {
+	sourceFile := os.Getenv("CONCOURSE_SOURCE_FILE")
+	if _, err := os.Stat(sourceFile); !os.IsNotExist(err) {
+		source = ConcourseSource{}
+
+		sourceYml, err := ioutil.ReadFile(sourceFile)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = yaml.Unmarshal(sourceYml, &source)
+		Expect(err).ToNot(HaveOccurred())
+
+	} else {
+		source = ConcourseSource{
+			ExternalURL: os.Getenv("CONCOURSE_URL"),
+			LocalUser: LocalUser{
+				Username: os.Getenv("CONCOURSE_USERNAME"),
+				Password: os.Getenv("CONCOURSE_PASSWORD"),
+			},
+			FlyPath: os.Getenv("FLY_PATH"),
+		}
+	}
+	skipSSLValidation, err := strconv.ParseBool(os.Getenv("FLY_SKIP_SSL"))
+	if err != nil {
+		skipSSLValidation = false
+	}
+	source.SkipSSLValidation = skipSSLValidation
+	if source.ExternalURL == "" {
 		Fail("CONCOURSE_URL is a required paramter")
 	}
-
-	username = os.Getenv("CONCOURSE_USERNAME")
-	password = os.Getenv("CONCOURSE_PASSWORD")
-
-	flyPath = os.Getenv("FLY_PATH")
-	if flyPath == "" {
-		flyPath = "fly"
-	}
-
-	if os.Getenv("FLY_SKIP_SSL") == "true" {
-		skipSSLValidation = true
+	if source.FlyPath == "" {
+		source.FlyPath = "fly"
 	}
 })
 
@@ -65,7 +88,7 @@ func spawn(argc string, argv ...string) *gexec.Session {
 }
 
 func spawnFly(argv ...string) *gexec.Session {
-	return spawn(flyPath, append([]string{"-t", flyTarget}, argv...)...)
+	return spawn(source.FlyPath, append([]string{"-t", flyTarget}, argv...)...)
 }
 
 func fly(argv ...string) *gexec.Session {
@@ -80,8 +103,8 @@ func wait(session *gexec.Session) {
 }
 
 func spawnFlyLogin(args ...string) *gexec.Session {
-	extraArgs := []string{"login", "-c", concourseUrl, "-u", username, "-p", password}
-	if skipSSLValidation {
+	extraArgs := []string{"login", "-c", source.ExternalURL, "-u", source.LocalUser.Username, "-p", source.LocalUser.Password}
+	if source.SkipSSLValidation {
 		extraArgs = append(extraArgs, "-k")
 	}
 
